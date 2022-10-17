dff3eb73e79859f2e07b41f82a6794fc1ab9af9c
JTAbeleda <jt.abeleda@knights.ucf.edu>
2022-10-17 19:18:27 -0400

    First half of last video done.
    TM pro added with packages and added additional Ui elements to the game.

--- Tutorial 2 Platformer/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2 Platformer/Assets/Scripts/PlayerScript.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerScript : MonoBehaviour
 {
 
     private Rigidbody2D rd2d;
     public float speed;
+    public TextMeshProUGUI score;
+    private int scoreValue = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
     }
 
     // Update is called once per frame
@@ -23,4 +28,15 @@ public class PlayerScript : MonoBehaviour
          rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+
+}
 }
