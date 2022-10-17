92da83622e9d450969ecd64e603731f0092e6f94
JTAbeleda <jt.abeleda@knights.ucf.edu>
2022-10-17 19:46:22 -0400

    Finished the assignment
    Added finishing touches to the code. Additionally, made sure everything worked fine.

--- Tutorial 2 Platformer/Assets/Scripts/PlayerScript.cs
+++ Tutorial 2 Platformer/Assets/Scripts/PlayerScript.cs
@@ -12,7 +12,7 @@ public class PlayerScript : MonoBehaviour
     public TextMeshProUGUI score;
     private int scoreValue = 0;
 
-    // Start is called before the first frame update
+        // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
@@ -20,13 +20,11 @@ public class PlayerScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
-        float verMovement = Input.GetAxis("Vertical");
-        
-         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
-
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,5 +36,17 @@ public class PlayerScript : MonoBehaviour
             Destroy(collision.collider.gameObject);
         }
 
-}
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+            }
+        }
+        
+    }
 }
